5b32c5855958b0876b29db927cb4f40bc7219ba9
herbeton <herbeton7@gmail.com>
2016-03-01 22:04:53 -0300

    Upando os arquivos referente a interface grafica(Incompleto)

---
+++ ControleInteligenteAEstrela/FormConfig.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ControleInteligenteAEstrela
+{
+    partial class FormConfig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNColunas = new System.Windows.Forms.TextBox();
+            this.textBoxCustoVertical = new System.Windows.Forms.TextBox();
+            this.textBoxCustoHorizontal = new System.Windows.Forms.TextBox();
+            this.labelNColunas = new System.Windows.Forms.Label();
+            this.textBoxCustoDiagonal = new System.Windows.Forms.TextBox();
+            this.labelCustoVertical = new System.Windows.Forms.Label();
+            this.labelCustoHorizontal = new System.Windows.Forms.Label();
+            this.labelCustoDiagonal = new System.Windows.Forms.Label();
+            this.textBoxPosicaoSaida = new System.Windows.Forms.TextBox();
+            this.textBoxPosicaoChegada = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNLinhas = new System.Windows.Forms.TextBox();
+            this.menuStripConfig = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adicionarArquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripConfig.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxNColunas
+            // 
+            this.textBoxNColunas.Location = new System.Drawing.Point(139, 119);
+            this.textBoxNColunas.Name = "textBoxNColunas";
+            this.textBoxNColunas.Size = new System.Drawing.Size(114, 20);
+            this.textBoxNColunas.TabIndex = 0;
+            // 
+            // textBoxCustoVertical
+            // 
+            this.textBoxCustoVertical.Location = new System.Drawing.Point(139, 145);
+            this.textBoxCustoVertical.Name = "textBoxCustoVertical";
+            this.textBoxCustoVertical.Size = new System.Drawing.Size(114, 20);
+            this.textBoxCustoVertical.TabIndex = 1;
+            // 
+            // textBoxCustoHorizontal
+            // 
+            this.textBoxCustoHorizontal.Location = new System.Drawing.Point(139, 171);
+            this.textBoxCustoHorizontal.Name = "textBoxCustoHorizontal";
+            this.textBoxCustoHorizontal.Size = new System.Drawing.Size(114, 20);
+            this.textBoxCustoHorizontal.TabIndex = 2;
+            // 
+            // labelNColunas
+            // 
+            this.labelNColunas.AutoSize = true;
+            this.labelNColunas.Location = new System.Drawing.Point(3, 122);
+            this.labelNColunas.Name = "labelNColunas";
+            this.labelNColunas.Size = new System.Drawing.Size(123, 13);
+            this.labelNColunas.TabIndex = 3;
+            this.labelNColunas.Text = "Numero de colunas (NC)";
+            // 
+            // textBoxCustoDiagonal
+            // 
+            this.textBoxCustoDiagonal.Location = new System.Drawing.Point(139, 197);
+            this.textBoxCustoDiagonal.Name = "textBoxCustoDiagonal";
+            this.textBoxCustoDiagonal.Size = new System.Drawing.Size(114, 20);
+            this.textBoxCustoDiagonal.TabIndex = 4;
+            // 
+            // labelCustoVertical
+            // 
+            this.labelCustoVertical.AutoSize = true;
+            this.labelCustoVertical.Location = new System.Drawing.Point(3, 148);
+            this.labelCustoVertical.Name = "labelCustoVertical";
+            this.labelCustoVertical.Size = new System.Drawing.Size(109, 13);
+            this.labelCustoVertical.TabIndex = 5;
+            this.labelCustoVertical.Text = "Custo na vertical (CV)";
+            // 
+            // labelCustoHorizontal
+            // 
+            this.labelCustoHorizontal.AutoSize = true;
+            this.labelCustoHorizontal.Location = new System.Drawing.Point(3, 174);
+            this.labelCustoHorizontal.Name = "labelCustoHorizontal";
+            this.labelCustoHorizontal.Size = new System.Drawing.Size(121, 13);
+            this.labelCustoHorizontal.TabIndex = 6;
+            this.labelCustoHorizontal.Text = "Custo na horizontal (CH)";
+            // 
+            // labelCustoDiagonal
+            // 
+            this.labelCustoDiagonal.AutoSize = true;
+            this.labelCustoDiagonal.Location = new System.Drawing.Point(3, 200);
+            this.labelCustoDiagonal.Name = "labelCustoDiagonal";
+            this.labelCustoDiagonal.Size = new System.Drawing.Size(113, 13);
+            this.labelCustoDiagonal.TabIndex = 7;
+            this.labelCustoDiagonal.Text = "Custo na diagonal(CD)";
+            // 
+            // textBoxPosicaoSaida
+            // 
+            this.textBoxPosicaoSaida.Location = new System.Drawing.Point(8, 56);
+            this.textBoxPosicaoSaida.Name = "textBoxPosicaoSaida";
+            this.textBoxPosicaoSaida.ReadOnly = true;
+            this.textBoxPosicaoSaida.Size = new System.Drawing.Size(118, 20);
+            this.textBoxPosicaoSaida.TabIndex = 8;
+            // 
+            // textBoxPosicaoChegada
+            // 
+            this.textBoxPosicaoChegada.Location = new System.Drawing.Point(136, 56);
+            this.textBoxPosicaoChegada.Name = "textBoxPosicaoChegada";
+            this.textBoxPosicaoChegada.ReadOnly = true;
+            this.textBoxPosicaoChegada.Size = new System.Drawing.Size(114, 20);
+            this.textBoxPosicaoChegada.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Posição de saida";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Posição de chegada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Numero de linhas (NL)";
+            // 
+            // textBoxNLinhas
+            // 
+            this.textBoxNLinhas.Location = new System.Drawing.Point(139, 93);
+            this.textBoxNLinhas.Name = "textBoxNLinhas";
+            this.textBoxNLinhas.Size = new System.Drawing.Size(114, 20);
+            this.textBoxNLinhas.TabIndex = 13;
+            // 
+            // menuStripConfig
+            // 
+            this.menuStripConfig.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStripConfig.Location = new System.Drawing.Point(0, 0);
+            this.menuStripConfig.Name = "menuStripConfig";
+            this.menuStripConfig.Size = new System.Drawing.Size(265, 24);
+            this.menuStripConfig.TabIndex = 14;
+            this.menuStripConfig.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adicionarArquivoToolStripMenuItem,
+            this.salvarToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // adicionarArquivoToolStripMenuItem
+            // 
+            this.adicionarArquivoToolStripMenuItem.Name = "adicionarArquivoToolStripMenuItem";
+            this.adicionarArquivoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.adicionarArquivoToolStripMenuItem.Text = "Abrir";
+            this.adicionarArquivoToolStripMenuItem.Click += new System.EventHandler(this.adicionarArquivoToolStripMenuItem_Click);
+            // 
+            // salvarToolStripMenuItem
+            // 
+            this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salvarToolStripMenuItem.Text = "Salvar";
+            this.salvarToolStripMenuItem.Click += new System.EventHandler(this.salvarToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // FormConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 229);
+            this.Controls.Add(this.textBoxNLinhas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPosicaoChegada);
+            this.Controls.Add(this.textBoxPosicaoSaida);
+            this.Controls.Add(this.labelCustoDiagonal);
+            this.Controls.Add(this.labelCustoHorizontal);
+            this.Controls.Add(this.labelCustoVertical);
+            this.Controls.Add(this.textBoxCustoDiagonal);
+            this.Controls.Add(this.labelNColunas);
+            this.Controls.Add(this.textBoxCustoHorizontal);
+            this.Controls.Add(this.textBoxCustoVertical);
+            this.Controls.Add(this.textBoxNColunas);
+            this.Controls.Add(this.menuStripConfig);
+            this.MainMenuStrip = this.menuStripConfig;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(281, 268);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(281, 268);
+            this.Name = "FormConfig";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configurações do labirinto";
+            this.menuStripConfig.ResumeLayout(false);
+            this.menuStripConfig.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNColunas;
+        private System.Windows.Forms.TextBox textBoxCustoVertical;
+        private System.Windows.Forms.TextBox textBoxCustoHorizontal;
+        private System.Windows.Forms.Label labelNColunas;
+        private System.Windows.Forms.TextBox textBoxCustoDiagonal;
+        private System.Windows.Forms.Label labelCustoVertical;
+        private System.Windows.Forms.Label labelCustoHorizontal;
+        private System.Windows.Forms.Label labelCustoDiagonal;
+        private System.Windows.Forms.TextBox textBoxPosicaoSaida;
+        private System.Windows.Forms.TextBox textBoxPosicaoChegada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNLinhas;
+        private System.Windows.Forms.MenuStrip menuStripConfig;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adicionarArquivoToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ ControleInteligenteAEstrela/FormConfig.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleInteligenteAEstrela
+{
+    public partial class FormConfig : Form
+    {
+        string diretorioArquivoTxt = "";
+        public FormConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void adicionarArquivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "txt files (*.txt)|*.txt";
+            openFile.ShowDialog();
+            diretorioArquivoTxt = openFile.FileName;
+            LerArquivoTxt(diretorioArquivoTxt);
+            //MessageBox.Show(diretorioArquivoTxt);
+        }
+
+        private void LerArquivoTxt(String diretorio)
+        {
+            StreamReader texto = new StreamReader(diretorio);
+            string primeiraLinhaArquivo;
+            List<string> bufferTexto = new List<string>();
+            string buffer = "";
+            int tamanhoLinhaInicial = 0;
+            int i = 0;
+            if ((primeiraLinhaArquivo = texto.ReadLine()) != null)
+            {
+                tamanhoLinhaInicial = primeiraLinhaArquivo.Length;
+                while(tamanhoLinhaInicial > i){
+                    if (primeiraLinhaArquivo[i].ToString() != " " && tamanhoLinhaInicial != i + 1)
+                    {
+                        buffer = buffer + primeiraLinhaArquivo[i];
+                    }
+                    else{
+                        if (tamanhoLinhaInicial == i + 1)
+                        {
+                            buffer = buffer + primeiraLinhaArquivo[i];
+                            bufferTexto.Add(buffer);
+                            buffer = "";
+                        }
+                        else
+                        {
+                            bufferTexto.Add(buffer);
+                            buffer = "";
+                        }
+                    }
+                    i++;
+                }
+            }
+            SetTextBoxs(bufferTexto);
+        }
+
+        private void SetTextBoxs(List<string> bufferTexto)
+        {
+            textBoxNLinhas.Text = bufferTexto[0];
+            textBoxNColunas.Text = bufferTexto[1];
+            textBoxCustoHorizontal.Text = bufferTexto[2];
+            textBoxCustoVertical.Text = bufferTexto[3];
+            bufferTexto.Clear();
+            textBoxCustoDiagonal.Text = CalculaCustoDiagonalAutomatico(Convert.ToInt32(textBoxCustoHorizontal.Text), 
+                Convert.ToInt32(textBoxCustoVertical.Text));
+        }
+
+        private string CalculaCustoDiagonalAutomatico(int CH, int CV)
+        {
+            return Math.Sqrt(Math.Pow(CH, 2) + Math.Pow(CV, 2)).ToString();
+        }
+
+        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.ShowDialog();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
